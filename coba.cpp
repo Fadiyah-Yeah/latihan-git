4705c639859abb73a286c634bf447fcb4c04b01a
Fadiyah-Yeah <124240041@student.upnyk.ac.id>
2025-07-21 11:29:54 +0700

    Add files via upload

---
+++ coba.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void penjumlahan(){
+	int angka1, angka2;
+	cout << "Masukkan Angka pertama:";
+	cin >> angka1;
+	cout << endl;
+	cout << "Masukkan Angka kedua: ";
+	cin >> angka2;
+	int jumlah= angka1 + angka2;
+	cout << "Hasil: " << jumlah;
+	}
+ 
+int main( )
+{
+	penjumlahan();
+	cout << endl;
+    cout << "Hello, World!" << endl;
+    return 0;
+}
